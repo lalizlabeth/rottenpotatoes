7a4d6a27838a09146a78b0a934fba062a1562ceb
saasbook <saasbook@saasbook.(none)>
2013-10-06 12:40:10 -0700

    Functional now

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,11 +7,27 @@ class MoviesController < ApplicationController
   end
 
   def index
-    sort = params[:sort]
-    @movies = Movie.all
-    if sort
-      @movies = Movie.order(sort)
+    sort = params[:sort] || session[:sort]
+    case sort
+      when 'title'
+        ordering, @title_header = {:order => :title}, 'active'
+      when 'release_date'
+        ordering, @release_date_header = {:order => :release_date}, 'active'
     end
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
+
+    if params[:sort] != session[:sort]
+      session[:sort] = sort
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+
+    if params[:ratings] != session[:ratings] and @selected_ratings != {}
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+    @movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
   end
 
   def new
